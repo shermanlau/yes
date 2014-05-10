d9a2956e010d905349c21581dba530b18edabbbb
admin <admin@Administrators-iMac-88.local>
2014-05-09 20:17:16 -0400

    Initial Commit

---
+++ Rectangle/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Rectangle
+//
+//  Created by admin on 2014-05-09.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Rectangle/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Rectangle
+//
+//  Created by admin on 2014-05-09.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
